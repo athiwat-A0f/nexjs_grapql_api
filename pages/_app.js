c5a16a18f0811b80372aebaf880fcedd3a7c8b14
athiwat8971 <athiwat.8971@gmail.com>
2021-03-20 10:42:10 +0700

    add new navbar react-bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,21 @@
 import Head from "next/head";
-import Nav from "../components/Nav";
+import NavMenu from "../components/NavMenu";
 import Footer from "../components/Footer";
 import "../styles/globals.css";
 import { Container } from "react-bootstrap";
+import "../assets/bootstrap4/bootstrap.min.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Head>
         <title>Next Sequelize</title>
-
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-          integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-          crossorigin="anonymous"
-        />
       </Head>
-      <Nav />
+      <NavMenu />
       <Container fluid="md">
-        <Component {...pageProps} />
+        <div className="col-12 text-center">
+          <Component {...pageProps} />
+        </div>
       </Container>
       <Footer />
     </div>
