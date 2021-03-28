29ede6a9b99c224369bf25d2d12034e804f1a739
athiwat8971 <athiwat.8971@gmail.com>
2021-03-28 22:14:11 +0700

    add loading ngprogress

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,14 @@ import Footer from "../components/Footer";
 import "../styles/globals.css";
 import "../assets/bootstrap4/bootstrap.min.css";
 import { Container } from "react-bootstrap";
+import Router from "next/router";
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
